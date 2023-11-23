727ec5a0d5a3bee7e9caec1429c76b05cddb1f8f
Waphyoo <nonny2246@gmail.com>
2023-11-23 08:35:08 +0700

    okok

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,19 @@ public class Main {
 
     public static void main(String[] args) {
         Stat wayu=new Stat(10,10,10);
+        Stat non=new Stat(5,5,5);
 
-        wayu.levelUp(1);
-        wayu.equipSheild(1);
-        wayu.equipSword(1);
+        wayu.equipSheild();
+        wayu.equipSword();
+        wayu.show();
+        non.attack(wayu);
+        wayu.show();
+        non.levelUp();
+        non.levelUp();
+        non.levelUp();
+        non.levelUp();
+        non.attack(wayu);
+        wayu.show();
+        non.show();
     }
 }
--- src/Stat.java
+++ src/Stat.java
@@ -1,30 +1,52 @@
 public class Stat {
-    private double maxHp;
-    private double maxMana;
-    private double damage;
-    private double def;
-    private double runSpeed;
+    double maxHp;
+    double maxMana;
+    double damage;
+    double def;
+    double runSpeed;
+
+    int level=0;
 
     public Stat(double damage,double def,double runSpeed){
-        this.maxHp=100;
-        this.maxMana=50;
+        maxHp=100;
+        maxMana=50;
         this.def=def;
         this.damage=damage;
         this.runSpeed=runSpeed;
 
     }
-    public void levelUp(int level){
-        this.maxHp=100+(10*level);
-        this.maxMana=100+(10*level);
-        this.damage*=(1+0.1*level);
-        this.def*=(1+0.05*level);
-        this.runSpeed*=(0.1+0.03*level);
+    public void levelUp(){
+        level++;
+        maxHp=100+(10*level);
+        maxMana=100+(10*level);
+        damage*=(1+0.1*level);
+        def*=(1+0.05*level);
+        runSpeed*=(0.1+0.03*level);
     }
 
-    public void equipSword(int level){
+    public void equipSword(){
         this.runSpeed-=(0.1+0.04*level);
+
+
     }
-    public void equipSheild(int level){
+    public void equipSheild(){
         this.runSpeed-=(0.1+0.08*level);
     }
+
+    public void show(){
+        System.out.println("////////////////////");
+        System.out.println("Hp"+this.maxHp);
+        System.out.println("Mana"+this.maxMana);
+        System.out.println("damage"+this.damage);
+        System.out.println("def"+this.def);
+        System.out.println("level"+this.level);
+        System.out.println("reunSpeed"+this.runSpeed);
+
+    }
+
+    public double attack(Stat op){
+        if(op.def>=damage)return op.maxHp;
+        return op.maxHp=op.maxHp+op.def-damage;
+    }
+
 }
